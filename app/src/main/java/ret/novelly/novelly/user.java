d93f78cf2f603de62a39579f6c1b8d5a98f91a9a
ret702 <ret702@gmail.com>
2015-09-03 20:11:25 -0700

    new

---
+++ app/src/main/java/ret/novelly/novelly/user.java
@@ -0,0 +1,26 @@
+package ret.novelly.novelly;
+
+import java.util.UUID;
+
+/**
+ * Created by ret70 on 9/3/2015.
+ */
+
+public class user {
+
+    public void createUser()
+    {
+        UUID test = UUID.randomUUID();
+    }
+    public String getStory()
+    {
+
+
+        return "";
+    }
+
+    public void  submitStory(String story, UUID userID)
+    {
+
+    }
+}
